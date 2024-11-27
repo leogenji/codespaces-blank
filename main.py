51f7c12478ae62a63e20d6497880c3d8a25cf7ed
leogenji <135902848+leogenji@users.noreply.github.com>
2024-11-27 21:13:05 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#lee la variable y le asigna un valor y la guarda en memoria
+nombre = input("ingresa tu nombre: ") #imprime un mensaje al usuario y espera que ingrese un dato
+apellido = input("ingresa tu apellido: ")
+ciudad = input("ingresa tu ciudad: ")
+
+print(f"Hola, {nombre} {apellido}. Seleccionaste la ciudad: {ciudad}. ")
+
+nombre_completo = f"{nombre} {apellido}"
+
+print(f"nombre completo: {nombre_completo}")
